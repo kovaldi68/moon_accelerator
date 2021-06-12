ceeb3ae2d52c3f3e9702965270038d8a3763b429
Dmitry Kovalenko <dmitry.kovalenko.mu@gmail.com>
2021-06-12 19:34:54 +0300

    header+promo blocks for all versions

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,30 @@
 'use strict';
+
 var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var menuToggle = document.querySelector('.page-header__toggle');
+const mediaDesktop = window.matchMedia('(min-width: 1024px)');
 
 pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+const onMenuHandler = (evt) => {
+  evt.preventDefault();
+  const headerHeight = pageHeader.offsetHeight;
+
+  if (pageHeader.classList.contains('page-header--opened')) {
+    pageHeader.classList.remove('page-header--opened')
+    document.body.style.paddingTop = 0;
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add('page-header--opened')
+    document.body.style.paddingTop = `${headerHeight}px`;
   }
-});
+};
+
+const closeHeader = () => {
+  if (mediaDesktop.matches) {
+    document.body.style.paddingTop = 0;
+    pageHeader.classList.remove("page-header--opened");
+  }
+}
+
+menuToggle.addEventListener('click', onMenuHandler);
+closeHeader();
