afe4c030b07d74b5122df90cd5e10ba4a3d420ca
Dmitry Kovalenko <dmitry.kovalenko.mu@gmail.com>
2021-06-14 18:54:52 +0300

    Update 2021-06-14T15:54:12.643Z

--- js/script.js
+++ js/script.js
@@ -1,10 +1,47 @@
 'use strict';
 
-var pageHeader = document.querySelector('.page-header');
-var menuToggle = document.querySelector('.page-header__toggle');
+const pageHeader = document.querySelector('.page-header');
+const menuToggle = document.querySelector('.page-header__toggle');
 const mediaDesktop = window.matchMedia('(min-width: 1024px)');
+const anchors = document.querySelectorAll('a[href*="#"]');
+const submitButton = document.querySelector('.button--submit');
+const body = document.body;
+const nameInput = document.querySelector('.form__input--name');
+const phoneInput = document.querySelector('.form__input--phone');
+
+
+const ALERT_SHOW_TIME = 3000;
 
 pageHeader.classList.remove('page-header--nojs');
+
+const onSubmitButtonHandler = function(e) {
+  e.preventDefault();
+
+  // const message = document.createElement('div');
+
+  // message.style.zIndex = 100;
+  // message.style.position = 'absolute';
+  // message.style.left = 0;
+  // message.style.top = 0;
+  // message.style.right = 0;
+  // message.style.color = 'white';
+  // message.style.padding = '16px 3px';
+  // message.style.fontSize = '15px';
+  // message.style.textAlign = 'center';
+  // message.style.backgroundColor = '#0ad9c6';
+
+  // message.textContent = 'Форма успешно отправлена!';
+  // body.appendChild(message);
+
+  // setTimeout(() => {
+  //   message.remove();
+  // }, ALERT_SHOW_TIME);
+
+  if (isStorageSupport) {
+    localStorage.setItem('userName', nameInput.value);
+    localStorage.setItem('userPhone', phoneInput.value);
+  }
+}
 
 const onMenuHandler = (evt) => {
   evt.preventDefault();
@@ -26,5 +63,42 @@ const closeHeader = () => {
   }
 }
 
+const scrollSmooth = () => {
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const blockID = anchor.getAttribute('href').substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    })
+  }
+}
+
+//localStorage
+
+let isStorageSupport = true;
+let storageName = '';
+let storagePhone = '';
+
+try {
+  storageName = localStorage.getItem('userName');
+  storagePhone = localStorage.getItem('userPhone');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+const storageData = () => {
+  if (storageName && storagePhone) {
+    nameInput.value = storageName;
+    phoneInput.value = storagePhone;
+  }
+}
+
+// submitButton.addEventListener('click', onSubmitButtonHandler);
 menuToggle.addEventListener('click', onMenuHandler);
 closeHeader();
+scrollSmooth();
