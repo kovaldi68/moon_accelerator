2ee0fb5ef4e0fafa726585d95abd075e34f9b804
kovaldi68 <dmitry.kovalenko.mu@gmail.com>
2021-06-26 01:04:17 +0300

    Fixes after test review

--- build/js/main.js
+++ build/js/main.js
@@ -23,18 +23,20 @@
   const menuToggle = document.querySelector('.page-header__toggle');
   const mediaDesktop = window.matchMedia('(min-width: 1024px)');
   const body = document.body;
-  
+
   pageHeader.classList.remove('page-header--nojs');
-  
+
   const onMenuHandler = (evt) => {
     evt.preventDefault();
     const headerHeight = pageHeader.offsetHeight;
   
     if (pageHeader.classList.contains('page-header--opened')) {
       pageHeader.classList.remove('page-header--opened')
+      body.classList.remove('page-body--menu-opened')
       body.style.paddingTop = 0;
     } else {
       pageHeader.classList.add('page-header--opened')
+      body.classList.add('page-body--menu-opened')
       body.style.paddingTop = `${headerHeight}px`;
     }
   };
@@ -43,6 +45,7 @@
     if (mediaDesktop.matches) {
       body.style.paddingTop = 0;
       pageHeader.classList.remove('page-header--opened');
+      body.classList.remove('page-body--menu-opened')
     }
   };
   
